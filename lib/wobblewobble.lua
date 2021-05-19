aebd1683f5bc11dbaa83abe6fe0bf4edaf589898
Zack Scholl <zack.scholl@gmail.com>
2021-05-19 12:06:35 -0700

    formatting

--- lib/wobblewobble.lua
+++ lib/wobblewobble.lua
@@ -10,33 +10,33 @@ function Wobble:new(args)
 end
 
 function Wobble:grid_init()
-    for i=2,5 do
-        self.g:led(1,i,15)
-    end
-    self.g:refresh()
+  for i=2,5 do
+    self.g:led(1,i,15)
+  end
+  self.g:refresh()
 end
 
 function Wobble:grid_key(x,y,z)
-    local currentout=params:get("crow")
+  local currentout=params:get("crow")
 
-    if x == 1 and y == 1 and z == 1 then
-        self.tog = 1 - self.tog
-        self.g:led(1, 1, 15 * self.tog)
+  if x==1 and y==1 and z==1 then
+    self.tog=1-self.tog
+    self.g:led(1,1,15*self.tog)
+  end
+
+  if y>1 and y<6 then
+    local crowmodkey=(y-1).."modulation"
+    local currentmod=params:get(crowmodkey)
+
+    if (x>0 and x<=#self.modulations) then
+      self.g:led(currentmod,y,0)
+      self.g:led(x,y,15)
+      params:set(crowmodkey,x)
+      params:set("crow",y-1)
     end
+  end
 
-    if y > 1 and y < 6 then
-        local crowmodkey = (y - 1).."modulation"
-        local currentmod = params:get(crowmodkey)
-
-        if (x > 0 and x <= #self.modulations) then
-            self.g:led(currentmod, y, 0)
-            self.g:led(x, y, 15)
-            params:set(crowmodkey, x)
-            params:set("crow", y - 1)
-        end
-    end
-
-    self.g:refresh()
+  self.g:refresh()
 end
 
 function Wobble:rebuild_menu(v)
@@ -59,12 +59,12 @@ function Wobble:rebuild_menu(v)
       end
     end
   end
-  for i=1,4 do 
+  for i=1,4 do
     if params:get(i.."midiin")==1 then
-        for _,param_name in ipairs(self.midi_names) do
-          params:hide(i..param_name)
-        end
-        params:hide(i.."miditype")
+      for _,param_name in ipairs(self.midi_names) do
+        params:hide(i..param_name)
+      end
+      params:hide(i.."miditype")
     end
   end
 end
@@ -75,7 +75,7 @@ function Wobble:init()
   self.midi_names={"level","attack","decay","sustain","release"}
   self.midi_types={"envelope","any note","top note"}
   -- setup modulations
-  self.modulations={"constant","sine","triangle","wobbly sine","snek","lorenz","henon","random walk","latoocarfian", "fbsine", "quad", "standardmap", "cookie"}
+  self.modulations={"constant","sine","triangle","wobbly sine","snek","lorenz","henon","random walk","latoocarfian","fbsine","quad","standardmap","cookie"}
   self.outputs={"none"}
   self.input1=0
   self.input2=0
@@ -88,7 +88,7 @@ function Wobble:init()
   end
   self:grid_init()
 
-  for _, mod in ipairs(self.modulations) do 
+  for _,mod in ipairs(self.modulations) do
     table.insert(self.outputs,mod)
   end
 
@@ -116,16 +116,16 @@ function Wobble:init()
       local connection=midi.connect(dev.port)
       local notes={}
       connection.event=function(data)
-        for i=1,4 do 
+        for i=1,4 do
           if self.mididevice_list[params:get(i.."midiin")]==name then
             -- process midi
             local d=midi.to_msg(data)
-            if d.type=="note_on" then 
+            if d.type=="note_on" then
               print(i,name,"midi on",d.note)
-              local highest_note=true 
-              if params:get(i.."miditype")==3 then -- top note 
+              local highest_note=true
+              if params:get(i.."miditype")==3 then -- top note
                 for k,_ in pairs(notes) do
-                  if k>d.note then 
+                  if k>d.note then
                     highest_note=false
                     break
                   end
@@ -135,12 +135,12 @@ function Wobble:init()
                 engine.setmidi(i,d.note)
               end
               notes[d.note]=true
-            elseif d.type=="note_off" then 
+            elseif d.type=="note_off" then
               print(i,name,"midi off",d.note)
               notes[d.note]=nil
               local have_notes=false
-              for k,v in pairs(notes) do 
-                have_notes=true 
+              for k,v in pairs(notes) do
+                have_notes=true
                 break
               end
               if not have_notes then
@@ -166,8 +166,7 @@ function Wobble:init()
   params:add{type="option",id="crow",name="crow",options={"1","2","3","4"},default=1,action=function(v)
     self:rebuild_menu(v)
     _menu.rebuild_params()
-  end
-  }
+  end}
   for i=1,4 do
     params:add{type="option",id=i.."modulation",name="modulation",options=self.modulations,default=1,action=function(v)
       engine.mod(i,v,
@@ -180,95 +179,80 @@ function Wobble:init()
         params:get(i.."sustain"),
         params:get(i.."release"),
         params:get(i.."meta"),
-        params:get(i.."meta2")
-      )
+      params:get(i.."meta2"))
       self:setmidi(i)
-    end
-    }
+    end}
     local do_update=true
     params:add{type="control",id=i.."period",name="period",controlspec=controlspec.new(0,300,'lin',0,1,'s',0.01/300),action=function(v)
-      if not do_update then 
-        do return end 
+      if not do_update then
+        do return end
       end
       do_update=false
       params:set(i.."freq",1/v)
       engine.hz(i,1/v)
       do_update=true
-    end
-    }
+    end}
     params:add{type="control",id=i.."freq",name="freq",controlspec=controlspec.new(0,300,'lin',0,1,'hz',0.01/300),action=function(v)
-      if not do_update then 
-        do return end 
+      if not do_update then
+        do return end
       end
       do_update=false
       params:set(i.."period",1/v)
       engine.hz(i,v)
       do_update=true
-    end
-    }
+    end}
 
     params:add{type="control",id=i.."meta",name="meta",controlspec=controlspec.new(-300,300,'lin',0,0,'metas',0.01/300),action=function(v)
-      if not do_update then 
-        do return end 
+      if not do_update then
+        do return end
       end
       do_update=false
       engine.meta(i,v)
       do_update=true
-    end
-    }
+    end}
 
     params:add{type="control",id=i.."meta2",name="meta2",controlspec=controlspec.new(-300,300,'lin',0,0,'metas',0.01/300),action=function(v)
-      if not do_update then 
-        do return end 
+      if not do_update then
+        do return end
       end
       do_update=false
       engine.meta(i,v)
       do_update=true
-    end
-    }
+    end}
 
     params:add{type="control",id=i.."minval",name="lfo min",controlspec=controlspec.new(-5,10,'lin',0,0,'',0.01/15),action=function(v)
       engine.minval(i,v)
-    end
-    }
+    end}
     params:add{type="control",id=i.."maxval",name="lfo max",controlspec=controlspec.new(-5,10,'lin',0,5,'',0.01/15),action=function(v)
       engine.maxval(i,v)
-    end
-    }
+    end}
     params:add{type="control",id=i.."clampmin",name="clamp min",controlspec=controlspec.new(-5,10,'lin',0,-5,'',0.01/15)}
     params:add{type="control",id=i.."clampmax",name="clamp max",controlspec=controlspec.new(-5,10,'lin',0,10,'',0.01/15)}
     params:add{type="option",id=i.."midiin",name="midi input",options=self.mididevice_list,default=1,action=function(v)
       self:setmidi(i)
       self:rebuild_menu(i)
       _menu.rebuild_params()
-    end
-    }
+    end}
     params:add{type="option",id=i.."miditype",name="midi type",options=self.midi_types,default=1,action=function(v)
       self:setmidi(i)
       self:rebuild_menu(i)
       _menu.rebuild_params()
-    end
-    }
+    end}
     params:add{type="control",id=i.."level",name="level",controlspec=controlspec.new(0,10,'lin',0,5,'v',0.1/10),action=function(v)
-      engine.level(i,v)      
-    end
-    }
+      engine.level(i,v)
+    end}
     params:add{type="control",id=i.."attack",name="attack",controlspec=controlspec.new(0,10,'lin',0,0.2,'s',0.01/10),action=function(v)
-      engine.attack(i,v)      
-    end
-    }
+      engine.attack(i,v)
+    end}
     params:add{type="control",id=i.."decay",name="decay",controlspec=controlspec.new(0,10,'lin',0,0.5,'s',0.01/10),action=function(v)
-      engine.decay(i,v)      
-    end
-    }
+      engine.decay(i,v)
+    end}
     params:add{type="control",id=i.."sustain",name="sustain",controlspec=controlspec.new(0,100,'lin',0,100,'%',1/100),action=function(v)
-      engine.sustain(i,v/100)      
-    end
-    }
+      engine.sustain(i,v/100)
+    end}
     params:add{type="control",id=i.."release",name="release",controlspec=controlspec.new(0,10,'lin',0,0.5,'s',0.01/10),action=function(v)
-      engine.release(i,v)      
-    end
-    }
+      engine.release(i,v)
+    end}
   end
 
   -- setup crow
@@ -277,53 +261,54 @@ function Wobble:init()
   end
 
   crow.input[1].mode("stream")
-  crow.input[1].stream = function(v)
-      self.input1 = v
+  crow.input[1].stream=function(v)
+    self.input1=v
 
-      do_update=false
-      curfreq = params:get("1freq")
-      engine.hz(1,v+curfreq)
-      do_update=true
+    do_update=false
+    curfreq=params:get("1freq")
+    engine.hz(1,v+curfreq)
+    do_update=true
   end
   crow.input[2].mode("stream")
-  crow.input[2].stream = function(v)
-      self.input2 = v
+  crow.input[2].stream=function(v)
+    self.input2=v
 
-      do_update=false
-      curfreq = params:get("2freq")
-      engine.hz(2,v+curfreq)
-      do_update=true
+    do_update=false
+    curfreq=params:get("2freq")
+    engine.hz(2,v+curfreq)
+    do_update=true
   end
 
   -- setup osc
   self.wf={}
-  for i=1,4 do 
+  for i=1,4 do
     self.wf[i]={}
-    for j=1,128 do 
+    for j=1,128 do
       self.wf[i][j]=0
     end
   end
   osc.event=function(path,args,from)
-   if path=="wobble" then
-     local i=tonumber(args[1])
-     if i~=nil and i>=1 and i<=4 then
-       for j=1,127 do
-         self.wf[i][j]=self.wf[i][j+1]
-       end
-       local volts=util.clamp(tonumber(args[2]),params:get(i.."clampmin"),params:get(i.."clampmax"))
-       crow.output[i].volts=volts
-       self.wf[i][128]=math.floor(util.linlin(-5,10,64,1,volts))
-     end
-   end
+    if path=="wobble" then
+      local i=tonumber(args[1])
+      if i~=nil and i>=1 and i<=4 then
+        for j=1,127 do
+          self.wf[i][j]=self.wf[i][j+1]
+        end
+        local volts=util.clamp(tonumber(args[2]),params:get(i.."clampmin"),params:get(i.."clampmax"))
+        crow.output[i].volts=volts
+        self.wf[i][128]=math.floor(util.linlin(-5,10,64,1,volts))
+      end
+    end
   end
+
 end
 
 function Wobble:setmidi(i)
-  if params:get(i.."midiin")==1 then 
+  if params:get(i.."midiin")==1 then
     print("adding nothing")
     engine.addenv(i,0)
     engine.addmidi(i,0)
-  elseif params:get(i.."miditype")==1 then 
+  elseif params:get(i.."miditype")==1 then
     print("adding env")
     engine.addenv(i,1)
     engine.addmidi(i,0)
@@ -337,7 +322,7 @@ end
 function Wobble:get()
   local i=params:get("crow")
   local midiname=self.mididevice_list[params:get(i.."midiin")]
-  if midiname=="none" then 
+  if midiname=="none" then
     midiname=""
   end
   return {
